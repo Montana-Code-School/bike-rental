85ce1583917d25ab76d759447f23eafaab02434c
Joie Cosby <jc283108@gmail.com>
2018-08-09 12:01:36 -0600

    added new google api to prod.env

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   STRIPE_KEY: '"pk_test_92VzagrJ1aBSy1iwm72RK2yx"',
-  // GOOGLE_MAPS_KEY: '"AIzaSyC7poZKe4fR1M0PXE6dYr-VA714b2qgBoc"'
+  GOOGLE_MAPS_KEY: '"AIzaSyAXQIZp8RciRwy3Rg9yVP_3SBVZ6UB295k"'
 }
